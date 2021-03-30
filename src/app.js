27191822688b7c6486758bee0869d923fabefedd
caioesqueletron <caiocaio620@gmail.com>
2021-03-29 23:44:25 -0300

    solution improved

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid, validate: isUuid } = require("uuid");
 
 const app = express();
 
@@ -9,6 +9,16 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateRepositorieId(request,response,next){
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+      return response.status(400).json({error:"Invalid repositorie ID"})
+  }
+
+  return next()
+}
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -30,7 +40,7 @@ app.post("/repositories", (request, response) => {
   return response.json(newRepository);
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id",validateRepositorieId, (request, response) => {
   const { title, url, techs } = request.body;
   const { id } = request.params;
 
@@ -52,7 +62,7 @@ app.put("/repositories/:id", (request, response) => {
 
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", validateRepositorieId,(request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
@@ -66,7 +76,7 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like",validateRepositorieId, (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
